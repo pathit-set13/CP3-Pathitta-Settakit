cc3f8a55c33d615f2679a54cc7a3d7a3c4fe0d26
Lazan113 <106540207+Lazan113@users.noreply.github.com>
2022-06-08 21:43:57 +0700

    Create Excercise 4
    Excercise 4 for python course by Bornto Dev

---
+++ Exercise4_Pathitta_S.py
@@ -0,0 +1,10 @@
+FoundEng = 79.4
+GenBus = 81.5
+IntCom = 75.0
+ComPro = 74.8
+# Out Put
+print("---Your Score---")
+print("Foundation English :","               ", FoundEng)
+print("General Business :","                 ", GenBus)
+print("Introduction to Computer Systems :"," ", IntCom)
+print("Computer Programming :","             ", ComPro)
