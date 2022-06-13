9b01693ff74f7736058d0bf62873fbb89ab86486
Lazan113 <106540207+Lazan113@users.noreply.github.com>
2022-06-13 22:11:08 +0700

    Create Exercise5.2
    Exercise5.2 for Python Course by BorntoDev

---
+++ Exercise5_2_Pathitta_S.py
@@ -0,0 +1,5 @@
+s = int(input("PLease input distance (km): "))
+t = int(input("Please input time (hr): "))
+v = s/t
+
+print("Speed :", v, "km/hr", "...or...", v*5/18, "m/s") #Add convert km/h to m/s
