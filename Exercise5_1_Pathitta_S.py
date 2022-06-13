232df80189eb7ce8366483536595e33070a51595
Lazan113 <106540207+Lazan113@users.noreply.github.com>
2022-06-13 21:51:43 +0700

    Create Exercise 5.1
    Exercise of Python 3 Course by BorntoDev

---
+++ Exercise5_1_Pathitta_S.py
@@ -0,0 +1,7 @@
+num_a = int(input("Input the first number: "))
+num_b = int(input("Input the second number: "))
+print("*** Result ***")
+print(num_a, "+", num_b, "=", num_a+num_b) #Plus
+print(num_a, "-", num_b, "=",num_a-num_b)  #Minus
+print(num_a, "*", num_b, "=",num_a*num_b)  #Multiply
+print(num_a, "/", num_b, "=",num_a/num_b)  #Divide
