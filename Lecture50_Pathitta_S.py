871037e9cc28ff2cd153586f347cbd0e4f4ce359
Lazan113 <106540207+Lazan113@users.noreply.github.com>
2022-07-06 23:31:47 +0700

    Create Homework Lecture 50
    Homework of lecture 50 form Python Course by BorntoDev

---
+++ Lecture50_Pathitta_S.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print("Plus: ",x+y)
+def minusNumber(x,y):
+    print("Minus: ", x-y)
+def multiplyNumber(x,y):
+    print("Multiply: ", x*y)
+def divideNumber(x,y):
+    print("Divide: ", x/y)
+
+x = int(input("x = "))
+y = int(input("y = "))
+addNumber(x,y)
+minusNumber(x,y)
+multiplyNumber(x,y)
+divideNumber(x,y)
